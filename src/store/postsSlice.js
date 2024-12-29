82e6e4c20601a0ecf8749f9b16dcf643a495acc3
Chetan Barange <chetanbarangecb@gmail.com>
2024-12-29 23:52:46 +0530

    Update postsSlice.js

--- src/store/postsSlice.js
+++ src/store/postsSlice.js
@@ -1,37 +1,3 @@
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
-
-// export const fetchPosts = createAsyncThunk('posts/fetchPosts', async (_, { rejectWithValue }) => {
-//   try {
-//     const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-//     return response.data.filter(post => post.id % 2 === 0);
-//   } catch (error) {
-//     return rejectWithValue(error.message);
-//   }
-// });
-
-// const postsSlice = createSlice({
-//   name: 'posts',
-//   initialState: { data: [], loading: false, error: null },
-//   extraReducers: builder => {
-//     builder
-//       .addCase(fetchPosts.pending, state => {
-//         state.loading = true;
-//         state.error = null;
-//       })
-//       .addCase(fetchPosts.fulfilled, (state, action) => {
-//         state.loading = false;
-//         state.data = action.payload;
-//       })
-//       .addCase(fetchPosts.rejected, (state, action) => {
-//         state.loading = false;
-//         state.error = action.payload;
-//       });
-//   },
-// });
-
-// export default postsSlice.reducer;
-// src/store/postsSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
